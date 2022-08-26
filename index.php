28c7950f7344978c129b109749addb518c16f020
Inder-insan <112197412+Inder-insan@users.noreply.github.com>
2022-08-26 23:59:18 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,167 @@
+<?php
+session_start();
+if (isset($_SESSION['$UserName_job'])) {
+} else {
+  header('location:../index.php');
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="cs" lang="cs">
+
+<head>
+  <meta http-equiv="content-type" content="text/html; charset=utf-8" />
+  <meta http-equiv="content-language" content="cs" />
+  <meta name="robots" content="all,follow" />
+
+  <meta name="author" content="All: ... [Nazev webu - www.url.cz]; e-mail: info@url.cz" />
+  <meta name="copyright" content="Design/Code: Vit Dlouhy [Nuvio - www.nuvio.cz]; e-mail: vit.dlouhy@nuvio.cz" />
+
+  <title>JOB PORTAL</title>
+  <meta name="description" content="..." />
+  <meta name="keywords" content="..." />
+
+  <link rel="index" href="./" title="Home" />
+  <link rel="stylesheet" media="screen,projection" type="text/css" href="./css/main.css" />
+  <link rel="stylesheet" media="print" type="text/css" href="./css/print.css" />
+  <link rel="stylesheet" media="aural" type="text/css" href="./css/aural.css" />
+  <style type="text/css">
+    <!--
+    .style1 {
+      color: #000066;
+      font-weight: bold;
+    }
+    -->
+  </style>
+</head>
+
+<body id="www-url-cz">
+  <!-- Main -->
+  <div id="main" class="box">
+    <?php
+    include "Header.php"
+    ?>
+    <?php
+    include "menu.php"
+    ?>
+    <!-- Page (2 columns) -->
+    <div id="page" class="box">
+      <div id="page-in" class="box">
+
+        <div id="strip" class="box noprint">
+
+          <!-- RSS feeds -->
+          <hr class="noscreen" />
+
+          <!-- Breadcrumbs -->
+          <p id="breadcrumbs">&nbsp;</p>
+          <hr class="noscreen" />
+
+        </div> <!-- /strip -->
+
+        <!-- Content -->
+        <div id="content">
+
+
+          <!-- /article -->
+
+          <hr class="noscreen" />
+
+
+          <!-- /article -->
+
+          <hr class="noscreen" />
+
+          <!-- Article -->
+
+          <!-- /article -->
+
+          <hr class="noscreen" />
+
+          <!-- Article -->
+          <div class="article">
+            <h2><span>Welcome To Control Panel</span></h2>
+
+
+            <table width="100%" border="0">
+              <tr>
+                <td>
+                  <div align="center"><img src="design/Profile.png" alt="" width="64" height="64" /></div>
+                </td>
+
+                <td>
+                  <div align="center"><img src="design/Edu.png" alt="" width="64" height="64" /></div>
+                </td>
+
+                <td>
+                  <div align="center"><img src="design/Search.png" alt="" width="64" height="64" /></div>
+                </td>
+              </tr>
+
+              <tr>
+                <td bgcolor="#A0B9F3">
+                  <div align="center"><a href="Profile.php" style="text-decoration: none;"><strong>Profile</strong></a></div>
+                </td>
+
+                <td bgcolor="#A0B9F3">
+                  <div align="center"><a href="Education.php" style="text-decoration: none;"><strong>Education</strong></a></div>
+                </td>
+
+                <td bgcolor="#A0B9F3">
+                  <div align="center"><a href="SearchJob.php" style="text-decoration: none;"><strong>Search JOB</strong></a></div>
+                </td>
+              </tr>
+
+              <tr>
+                <td>
+                  <div align="center"><img src="design/Interview.png" alt="" width="64" height="64" /></div>
+                </td>
+
+                <td>
+                  <div align="center"><img src="design/Feedback.png" alt="" width="64" height="64" /></div>
+                </td>
+
+                <td>
+                  <div align="center"><img src="design/Log.png" alt="" width="64" height="64" /></div>
+                </td>
+              </tr>
+
+              <tr>
+                <td bgcolor="#A0B9F3">
+                  <div align="center"><a href="Walkin.php" style="text-decoration: none;"><strong>Walkin</strong></a></div>
+                </td>
+
+                <td bgcolor="#A0B9F3">
+                  <div align="center"><a href="Feedback.php" style="text-decoration: none;"><strong>Feedback</strong></a></div>
+                </td>
+
+                <td bgcolor="#A0B9F3">
+                  <div align="center"><a href="session_destroy.php" style="text-decoration: none;"><strong>Logout</strong></a></div>
+                </td>
+              </tr>
+
+            </table>
+            <p>&nbsp;</p>
+
+            <p class="btn-more box noprint">&nbsp;</p>
+          </div> <!-- /article -->
+
+          <hr class="noscreen" />
+
+        </div> <!-- /content -->
+
+        <?php
+        include "right.php"
+        ?>
+
+      </div> <!-- /page-in -->
+    </div> <!-- /page -->
+
+
+    <?php
+    include "footer.php"
+    ?>
+  </div> <!-- /main -->
+
+</body>
+
+</html>
